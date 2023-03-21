7916a90e4bbc5ce6bd65116d273ff1b18520df98
shoaziz17 <109843670+shoaziz17@users.noreply.github.com>
2023-03-21 17:29:28 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getPadTime } from './utilities/getPadTime';
+import sound1 from './utilities/sound1.mp3';
+import sound2 from './utilities/sound2.mp3';
+import sound3 from './utilities/sound3.mp3';
 
 function App() {
+
+  const [timeLeft, setTimeLeft] = useState(25*60);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const minutes = getPadTime(Math.floor(timeLeft / 60));
+  const seconds = getPadTime(timeLeft - minutes * 60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      isCounting &&
+        setTimeLeft((timeLeft) => (timeLeft >= 1 ? timeLeft - 1 : 0))
+    }, 1000);
+    if (timeLeft === 0) { setIsCounting(false) }
+    return () => {
+      clearInterval(interval);
+    }
+  }, [isCounting, timeLeft])
+
+
+  const start = () => {
+    if (timeLeft === 0) {
+      setTimeLeft(5 * 60)
+    }
+    setIsCounting(true);
+    new Audio(sound1).play();
+  }
+  const stop = () => {
+    setIsCounting(false)
+    new Audio(sound2).play();
+
+  }
+  const restart = () => {
+    setIsCounting(false)
+    setTimeLeft(25 * 60)
+    new Audio(sound3).play();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Cucumbero</h1>
+      <div className='timer'>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className='buttons'>
+        {isCounting ? <button onClick={stop}>Stop</button> : <button onClick={start}>Start</button>}
+        <button onClick={restart}>Restart</button>
+      </div>
     </div>
   );
 }
